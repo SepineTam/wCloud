4b908fae4704c8a7d14e802aee9b9520b63c7d06
SongTan <165980942+SepineTam@users.noreply.github.com>
2024-06-24 18:52:00 +0800

    add stopword func

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
-# 如果跑出来的图片中不显示中文，则把第19行的注释取消，并将20行进行注释
 import jieba
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-# 读取文件内容
+def load_stopwords(filepath='stop.txt'):
+    stopwords = set()
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            word = line.strip()
+            if word:  # 只添加非空的行
+                stopwords.add(word)
+    return stopwords
+
 def read_file(filename):
+    filename = f'in/{filename}.txt'
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
+
     return content
 
-# 使用 jieba 进行中文分词
-def jieba_cut(content):
+def cut(content, stopwords=load_stopwords()):
+    if stopwords is None:
+        stopwords = set()
     words = jieba.cut(content, cut_all=False)
-    return ' '.join(words)
+    filtered_words = [word for word in words if word not in stopwords]
+    return ' '.join(filtered_words)
 
-# 生成词云图
-def generate_wordcloud(text):
-    # wordcloud = WordCloud(font_path='simhei.ttf', width=800, height=400, background_color='white').generate(text)
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+def gen_fig(text, cloudname='wordcloud'):
+    wordcloud = WordCloud(
+        font_path='SimHei.ttf', width=800, height=400, background_color='white'
+        ).generate(text)
+    cloudname = f'out/{cloudname}.png'
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
-    plt.savefig('wordcloud.png')
-    plt.show()
+    plt.savefig(cloudname)
+    # plt.show()
 
-# 主函数
-def main():
-    content = read_file('interview.txt')
-    text = jieba_cut(content)
-    generate_wordcloud(text)
+def main(name):
+    content = read_file(name)
+    gen_fig(
+        text = cut(content),
+        cloudname = name
+    )
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main('test')
